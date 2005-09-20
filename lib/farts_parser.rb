dcf8efb5032e6945ead75f52c5eb476c8f661040
Tyche <jlambert@oh.rr.com>
2005-09-20 01:25:46 +0000

    create new packages for 2.5.1

--- lib/farts_parser.rb
+++ lib/farts_parser.rb
@@ -31,7 +31,7 @@ module Farts
 
   class Parser < Racc::Parser
 
-module_eval <<'..end lib/farts_parser.y modeval..id810a67d1ca', 'lib/farts_parser.y', 106
+module_eval <<'..end lib/farts_parser.y modeval..ide3b70dc60d', 'lib/farts_parser.y', 106
 
   def initialize
     @scope = {}
@@ -51,7 +51,7 @@ module_eval <<'..end lib/farts_parser.y modeval..id810a67d1ca', 'lib/farts_parse
     raise Racc::ParseError, "Error: #{@sc.lineno}:#{@sc.tokenpos} syntax error at '#{val}'"
   end
 
-..end lib/farts_parser.y modeval..id810a67d1ca
+..end lib/farts_parser.y modeval..ide3b70dc60d
 
 ##### racc 1.4.4 generates ###
 
