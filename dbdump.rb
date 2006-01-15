7f784be74db875b416de9691197e203d2bfe0672
Tyche <jlambert@oh.rr.com>
2006-01-15 11:31:48 +0000

    Added options parsing to database loader and dumper

--- dbdump.rb
+++ dbdump.rb
@@ -17,52 +17,129 @@
 
 $:.unshift "lib"
 require 'yaml'
+require 'optparse'
+require 'ostruct'
 require 'pp'
 require 'db/properties'
 require 'db/player'
 require 'db/room'
 require 'farts/farts_parser'
 
-$dbtop = 0
-$db = {}
-$count = 0
+#
+# TODO: Add Description Here
+#
+class Dumper
+  VERSION = "0.1.0"
 
-case ARGV[1]
-when 'sdbm'
-  require 'sdbm'
-  SDBM.open(ARGV[0], 0666) do |db|
-    db.each_value do |v|
-      o = YAML::load(v)
-      $dbtop = o.id if o.id > $dbtop
-      $db[o.id]=o
-      $count += 1
+  attr_accessor :opts
+  DATABASES = [:dbm, :gdbm, :sdbm]
+
+  #
+  # TODO: Add description here
+  #
+  def initialize
+    @opts = get_options
+    case @opts.dbtype
+    when :dbm
+      require 'dbm'
+    when :gdbm
+      require 'gdbm'
+    when :sdbm
+      require 'sdbm'
     end
+    @dbtop = 0
+    @db = {}
+    @count = 0
   end
-when 'gdbm'
-  require 'gdbm'
-  GDBM.open("#{ARGV[0]}.gdbm", 0666) do |db|
-    db.each_value do |v|
-      o = YAML::load(v)
-      $dbtop = o.id if o.id > $dbtop
-      $db[o.id]=o
-      $count += 1
+
+  #
+  # Processes command line arguments
+  #
+  def get_options
+
+    # The myopts specified on the command line will be collected in *myopts*.
+    # We set default values here.
+    myopts = OpenStruct.new
+    myopts.ifile = nil
+    myopts.ofile = nil
+    myopts.dbtype = nil
+
+    opts = OptionParser.new do |opts|
+      opts.banner = "Database Dumper #{VERSION}"
+      opts.separator ""
+      opts.separator "Usage: ruby #{$0} [options]"
+      opts.separator ""
+      opts.separator "Options:"
+      opts.on("-i", "--ifile FILE", String,
+              "Select the database file to read",
+              "  extension determined automatically") {|myopts.ifile|}
+      opts.on("-o", "--ofile FILE", String,
+              "Select the yaml file to write",
+              "  defaults to same as database") {|myopts.ofile|}
+      opts.on("-t", "--type DBTYPE", DATABASES,
+              "Select the database type - required (no default)",
+              "  One of: #{DATABASES.join(", ")}",
+              "    Example: -t gdbm") {|myopts.dbtype|}
+      opts.on_tail("-h", "--help", "Show this message") do
+        puts opts.help
+        exit
+      end
+      opts.on_tail("-v", "--version", "Show version") do
+        puts "Database Dumper #{VERSION}"
+        exit
+      end
     end
+
+    opts.parse!(ARGV)
+    raise(OptionParser::MissingArgument.new("-t")) if myopts.dbtype == nil
+    raise(OptionParser::ParseError, "Must specify input file!") if myopts.ifile.nil?
+    myopts.ofile = myopts.ifile.dup if myopts.ofile.nil?
+    myopts.ifile << ".gdbm" if myopts.dbtype == :gdbm
+    myopts.ofile << ".yaml"
+
+    return myopts
+  rescue OptionParser::ParseError
+    puts "ERROR - #{$!}"
+    puts "For help..."
+    puts " ruby #{$0} --help"
+    exit
   end
-when 'dbm'
-  require 'dbm'
-  DBM.open(ARGV[0], 0666) do |db|
-    db.each_value do |v|
-      o = YAML::load(v)
-      $dbtop = o.id if o.id > $dbtop
-      $db[o.id]=o
-      $count += 1
+
+  def store(v)
+    o = YAML::load(v)
+    @dbtop = o.id if o.id > @dbtop
+    @db[o.id]=o
+    @count += 1
+  end
+
+  #
+  # Launches the dumper
+  #
+  def run
+    case @opts.dbtype
+    when :sdbm
+      SDBM.open(@opts.ifile, 0666) do |db|
+        db.each_value {|v| store v}
+      end
+    when :gdbm
+      GDBM.open(@opts.ifile, 0666) do |db|
+        db.each_value {|v| store v}
+      end
+    when :dbm
+      DBM.open(@opts.ifile, 0666) do |db|
+        db.each_value {|v| store v}
+      end
     end
+
+    File.open(@opts.ofile,'wb') do |f|
+      YAML::dump(@db.values,f)
+    end
+
+    puts "Highest object in use   : #{@dbtop}"
+    puts "Count of objects dumped : #{@count}"
   end
+
 end
 
-File.open("#{ARGV[0]}.yaml",'w') do |f|
-  YAML::dump($db.values,f)
-end
+app = Dumper.new.run
 
-puts "Highest object in use: #{$dbtop}"
-puts "Count of objects dumped: #{$count}"
--- dbload.rb
+++ dbload.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #
-# file::    dbdump.rb
+# file::    dbload.rb
 # author::  Jon A. Lambert
 # version:: 2.7.0
 # date::    1/12/2006
@@ -12,50 +12,130 @@
 # See LICENSE file for additional information.
 #
 
-# This utility program dumps a database to yaml
+# This utility program loads a yaml file to a database
 #
 
 $:.unshift "lib"
 require 'yaml'
+require 'optparse'
+require 'ostruct'
 require 'pp'
 require 'db/properties'
 require 'db/player'
 require 'db/room'
 require 'farts/farts_parser'
 
-$dbtop = 0
-$db = {}
-$count = 0
+#
+# TODO: Add Description Here
+#
+class Loader
+  VERSION = "0.1.0"
 
-YAML::load_file("#{ARGV[0]}.yaml").each do |o|
-  $dbtop = o.id if o.id > $dbtop
-  $db[o.id]=o
-  $count += 1
+  attr_accessor :opts
+  DATABASES = [:dbm, :gdbm, :sdbm]
+
+  #
+  # TODO: Add description here
+  #
+  def initialize
+    @opts = get_options
+    case @opts.dbtype
+    when :dbm
+      require 'dbm'
+    when :gdbm
+      require 'gdbm'
+    when :sdbm
+      require 'sdbm'
+    end
+    @dbtop = 0
+    @db = {}
+    @count = 0
+  end
+
+  #
+  # Processes command line arguments
+  #
+  def get_options
+
+    # The myopts specified on the command line will be collected in *myopts*.
+    # We set default values here.
+    myopts = OpenStruct.new
+    myopts.ifile = nil
+    myopts.ofile = nil
+    myopts.dbtype = nil
+
+    opts = OptionParser.new do |opts|
+      opts.banner = "Database Dumper #{VERSION}"
+      opts.separator ""
+      opts.separator "Usage: ruby #{$0} [options]"
+      opts.separator ""
+      opts.separator "Options:"
+      opts.on("-i", "--ifile FILE", String,
+              "Select the yaml file to read",
+              "  defaults to same as database") {|myopts.ifile|}
+      opts.on("-o", "--ofile FILE", String,
+              "Select the database file to write",
+              "  extension determined automatically") {|myopts.ofile|}
+      opts.on("-t", "--type DBTYPE", DATABASES,
+              "Select the database type - required (no default)",
+              "  One of: #{DATABASES.join(", ")}",
+              "    Example: -t gdbm") {|myopts.dbtype|}
+      opts.on_tail("-h", "--help", "Show this message") do
+        puts opts.help
+        exit
+      end
+      opts.on_tail("-v", "--version", "Show version") do
+        puts "Database Dumper #{VERSION}"
+        exit
+      end
+    end
+
+    opts.parse!(ARGV)
+    raise(OptionParser::MissingArgument.new("-t")) if myopts.dbtype == nil
+    raise(OptionParser::ParseError, "Must specify input file!") if myopts.ifile.nil?
+    myopts.ofile = myopts.ifile.dup if myopts.ofile.nil?
+    myopts.ofile << ".gdbm" if myopts.dbtype == :gdbm
+    myopts.ifile << ".yaml"
+
+    return myopts
+  rescue OptionParser::ParseError
+    puts "ERROR - #{$!}"
+    puts "For help..."
+    puts " ruby #{$0} --help"
+    exit
+  end
+
+  #
+  # Launches the loader
+  #
+  def run
+
+    YAML::load_file(@opts.ifile).each do |o|
+      @dbtop = o.id if o.id > @dbtop
+      @db[o.id]=o
+      @count += 1
+    end
+
+    case @opts.dbtype
+    when :sdbm
+      SDBM.open(@opts.ofile, 0666) do |db|
+        @db.each {|k,v| db[k.to_s] = YAML::dump(v)}
+      end
+    when :gdbm
+      GDBM.open(@opts.ofile, 0666) do |db|
+        @db.each {|k,v| db[k.to_s] = YAML::dump(v)}
+      end
+    when :dbm
+      DBM.open(@opts.ofile, 0666) do |db|
+        @db.each {|k,v| db[k.to_s] = YAML::dump(v)}
+      end
+    end
+
+    puts "Highest object in use   : #{@dbtop}"
+    puts "Count of objects dumped : #{@count}"
+  end
+
 end
 
-case ARGV[1]
-when 'sdbm'
-  require 'sdbm'
-  SDBM.open(ARGV[0], 0666) do |db|
-    $db.each do |k,v|
-      db[k.to_s] = YAML::dump(v)
-    end
-  end
-when 'gdbm'
-  require 'gdbm'
-  GDBM.open("#{ARGV[0]}.gdbm", 0666) do |db|
-    $db.each do |k,v|
-      db[k.to_s] = YAML::dump(v)
-    end
-  end
-when 'dbm'
-  require 'dbm'
-  DBM.open(ARGV[0], 0666) do |db|
-    $db.each do |k,v|
-      db[k.to_s] = YAML::dump(v)
-    end
-  end
-end
+app = Loader.new.run
 
-puts "Highest object in use: #{$dbtop}"
-puts "Count of objects dumped: #{$count}"
